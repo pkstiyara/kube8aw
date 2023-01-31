9120de6222525c5f26f727503b2477899672b613
pkstiyara <pkstiyara@gmail.com>
2023-01-31 22:06:09 +0530

    Intial

--- servers.tf
+++ servers.tf
@@ -1,30 +1,30 @@
-#Bastion
-resource "aws_instance" "bastion" {
-  ami           = var.ami_id
-  instance_type = "t3.micro"
-  subnet_id = module.vpc.public_subnets[0]
-  associate_public_ip_address = "true"
-  security_groups = [aws_security_group.allow_ssh.id]
-  key_name          =   aws_key_pair.k8_ssh.key_name
-  ##https://github.com/hashicorp/terraform/issues/30134
-  user_data = <<-EOF
-                #!bin/bash
-                echo "PubkeyAcceptedKeyTypes=+ssh-rsa" >> /etc/ssh/sshd_config.d/10-insecure-rsa-keysig.conf
-                systemctl reload sshd
-                echo "${tls_private_key.ssh.private_key_pem}" >> /home/ubuntu/.ssh/id_rsa
-                chown ubuntu /home/ubuntu/.ssh/id_rsa
-                chgrp ubuntu /home/ubuntu/.ssh/id_rsa
-                chmod 600   /home/ubuntu/.ssh/id_rsa
-                echo "starting ansible install"
-                apt-add-repository ppa:ansible/ansible -y
-                apt update
-                apt install ansible -y
-                EOF
+# #Bastion
+# resource "aws_instance" "bastion" {
+#   ami           = var.ami_id
+#   instance_type = "t2.micro"
+#   subnet_id = module.vpc.public_subnets[0]
+#   associate_public_ip_address = "true"
+#   security_groups = [aws_security_group.allow_ssh.id]
+#   key_name          =   aws_key_pair.k8_ssh.key_name
+#   ##https://github.com/hashicorp/terraform/issues/30134
+#   user_data = <<-EOF
+#                 #!bin/bash
+#                 echo "PubkeyAcceptedKeyTypes=+ssh-rsa" >> /etc/ssh/sshd_config.d/10-insecure-rsa-keysig.conf
+#                 systemctl reload sshd
+#                 echo "${tls_private_key.ssh.private_key_pem}" >> /home/ubuntu/.ssh/id_rsa
+#                 chown ubuntu /home/ubuntu/.ssh/id_rsa
+#                 chgrp ubuntu /home/ubuntu/.ssh/id_rsa
+#                 chmod 600   /home/ubuntu/.ssh/id_rsa
+#                 echo "starting ansible install"
+#                 apt-add-repository ppa:ansible/ansible -y
+#                 apt update
+#                 apt install ansible -y
+#                 EOF
 
-  tags = {
-    Name = "Bastion"
-  }
-}
+#   tags = {
+#     Name = "Bastion"
+#   }
+# }
 
 #Master
 resource "aws_instance" "masters" {
--- vars.tf
+++ vars.tf
@@ -11,7 +11,7 @@ variable "ami_id" {
 
 variable "availability_zones" {
   type    = list(string)
-  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  default = ["us-east-1a"]
 }
 
 variable "vpc_cidr" {
@@ -21,22 +21,22 @@ variable "vpc_cidr" {
 
 variable "private_subnets" {
     type = list(string)
-    default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+    default = ["10.0.1.0/24"]
 }
 
 variable "public_subnets" {
     type = list(string)
-    default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+    default = ["10.0.101.0/24"]
 }
 
 variable "master_node_count" {
     type = number
-    default = 3
+    default = 1
 }
 
 variable "worker_node_count" {
     type = number
-    default = 3
+    default = 2
 }
 
 variable "ssh_user" {
@@ -51,5 +51,5 @@ variable "master_instance_type" {
 
 variable "worker_instance_type" {
     type = string
-    default = "t3.micro"
+    default = "t2.micro"
 }
